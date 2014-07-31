b68420b5fa327554e17b85114789cc53bd1fa1e2
Michael Gerdes <mgerdes11@gmail.com>
2014-07-30 21:00:25 -0400

    Added simply path finding robot

--- teams/pathFind/RobotPlayer.java
+++ teams/pathFind/RobotPlayer.java
@@ -6,341 +6,208 @@ import java.util.*;
 
 public class RobotPlayer {
 	
-	static RobotController rc;
-	static Direction allDirections[] = Direction.values();
-	static int map[][];
-	static boolean foundPath;
-	static int path[][];
+	public static RobotController rc;
+	public static int[][] map;
+	public static int width;
+	public static int height;
+	public static Direction[] directions = Direction.values();
 	
 	public static void run (RobotController rcin) {
 		rc = rcin;
+		width = rc.getMapWidth();
+		height = rc.getMapHeight();
 		
-		while (true) {
-			try {
+		try {
+			while (true) {			
 				if (rc.getType() == RobotType.SOLDIER) {
-					
-					if (rc.isActive()) {
-						
-						if (shoot());
-						else {
-							if (isHQPathFound()) {
-								if (rc.getLocation().distanceSquaredTo(rc.senseEnemyHQLocation()) < 50) {
-									if (rc.senseNearbyGameObjects(Robot.class, 100, rc.getTeam()).length > 4) {
-										if (moveWhereHQWants());
-										else {
-											moveRandomly();
-										}
-									}
-								} else {
-									if (moveWhereHQWants());
-									else {
-										moveRandomly();
-									}	
-								}
-							} else {
-								moveRandomly();							
-							}
-						}
-					}
-				} else if (rc.getType() == RobotType.HQ){
-					if (rc.isActive() && rc.canMove(Direction.NORTH) && rc.senseRobotCount() < GameConstants.MAX_ROBOTS){
-						rc.spawn(Direction.NORTH);
-					}
-					
-					if (map == null) {
-						createMap();
-						setHQPathToNotFound();
-					}
-					
-					if (rc.readBroadcast(10000) == -1) {
-						bfs(rc.senseEnemyHQLocation());
-						setHQPathToFound();
-					}
+					runSoldier();
+				} else if (rc.getType() == RobotType.HQ) {
+					runHQ();
 				}
-			} catch (GameActionException e) {
-				e.printStackTrace();
+				rc.yield();
 			}
-			rc.yield();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 	
-	public static void move(MapLocation location) throws GameActionException {
-		if (!rc.isActive()) {
-			return;
+	public static void runSoldier() throws GameActionException {
+		if (isHQPathFound()) {
+			moveWhereHQWants();
+		} else {
+			moveRandomly();
 		}
+	}
+
+	public static void runHQ() throws GameActionException {
+		spawn();
 		
-		Direction movingDirection = rc.getLocation().directionTo(location);
-		if (rc.canMove(movingDirection)) {
-			rc.move(movingDirection);
-		}	
-	}
-
-	public static boolean shoot() throws GameActionException {
-		if (rc.isActive()) {
-			Robot[] enemyRobots = rc.senseNearbyGameObjects(Robot.class, rc.getType().attackRadiusMaxSquared + 10, rc.getTeam().opponent());
-			if (enemyRobots.length > 0) {
-				Robot anEnemy = enemyRobots[rc.getRobot().getID() % enemyRobots.length];
-				RobotInfo anEnemyInfo = rc.senseRobotInfo(anEnemy);
-				if (anEnemyInfo.location.distanceSquaredTo(rc.getLocation()) < rc.getType().attackRadiusMaxSquared) {
-					rc.attackSquare(anEnemyInfo.location);
-					return true;
-				} else {
-					move(anEnemyInfo.location);
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-
-	
-	public static void bugMove(MapLocation goal) throws GameActionException {
-		createMap();
-		if (rc.isActive()) {
-			Direction directionToGoal = rc.getLocation().directionTo(goal);
-			if (rc.canMove(directionToGoal)) {
-				// move in direction of goal.
-				rc.move(directionToGoal);
-			} else {
-				// follow obstacle
-				MapLocation myLocation = rc.getLocation();
-				
-				int north = map[myLocation.x][myLocation.y - 1];
-				int south = map[myLocation.x][myLocation.y - 1];
-				int west = map[myLocation.x - 1][myLocation.y];
-				int east = map[myLocation.x + 1][myLocation.y];
-				
-				
-			}
-		}
-	}
-	
-	public static void setSoldierPathToNotFound() throws GameActionException {
-		foundPath = false;
-	}
-	
-	public static void setSoldierPathToFound() throws GameActionException {
-		foundPath = true;
-	}
-	
-	public static boolean isSoldierPathFound() throws GameActionException {
-		return foundPath;
-	}
-	
-	public static void setHQPathToNotFound() throws GameActionException {
-		rc.broadcast(10000, -1);
-	}
-	
-	public static void setHQPathToFound() throws GameActionException {
-		rc.broadcast(10000, 1);
-	}
-	
-	public static boolean isHQPathFound() throws GameActionException {
-		if (rc.readBroadcast(10000) == -1) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-	
-	public static boolean moveWhereHQWants() throws GameActionException {
-		Direction movingDirection = allDirections[rc.readBroadcast(locToInt(rc.getLocation()))];
-		if (rc.canMove(movingDirection)) {
-			rc.move(movingDirection);
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	public static boolean moveWhereSoldierWants() throws GameActionException {
-		Direction movingDirection = allDirections[path[rc.getLocation().x][rc.getLocation().y]];
-		if (rc.canMove(movingDirection)) {
-			rc.move(movingDirection);
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	public static boolean moveRandomly() throws GameActionException {
-		Direction randomDirection = allDirections[(int)(Math.random() * 8)];
-		if (rc.canMove(randomDirection)) {
-			rc.move(randomDirection);
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	public static void createMap() {
-		if (map == null) {
-			int height = rc.getMapHeight();
-			int width = rc.getMapWidth();
-			
-			map = new int[width][height];
-			
-			for (int x = 0; x < width; x++) {
-				for (int y = 0; y < height; y++) {
-					map[x][y] = rc.senseTerrainTile(new MapLocation(x,y)).ordinal(); // 2 can't pass, 1 is a road, 0 can pass
-				}
-			}
-		}
-	}
-	
-	public static int locToInt(int x, int y) {
-		return x * 100 + y;
-	}
-	
-	public static int locToInt(MapLocation location) {
-		return location.x * 100 + location.y;
-	}
-	
-	public static MapLocation intToLoc (int location) {
-		return new MapLocation(location / 100, location % 100);
-	}
-	
-	public static void bfsTo(MapLocation startLocation, MapLocation endLocation) throws GameActionException {
-		int width = rc.getMapWidth();
-		int height = rc.getMapHeight();
-
 		if (map == null) {
 			createMap();
+			printMap();
+			setHQPathNotFound();
 		}
-		if (path == null) {
-			path = new int[width][height];
-		}
-				
-		int startX = startLocation.x;
-		int startY = startLocation.y;
-		int endX = endLocation.x;
-		int endY = endLocation.y;
-		
-		int xOffsets[] = {-1, 0, 1, 0, -1, 1, 1, -1};
-		int yOffsets[] = {0, -1, 0, 1, 1, -1, 1, -1};
-		
-		LinkedList<Integer> q = new LinkedList<Integer>();
-		boolean visited[][] = new boolean[width][height];
-		int parent[][][] = new int[width][height][2];
-		
-		for (int i = 0; i < visited.length; i++) {
-			for (int j = 0; j < visited[0].length; j++) {
-				visited[i][j] = false;
-			}
-		}
-		
-		q.add(startX);
-		q.add(startY);
-		visited[startX][startY] = true;
-		
-		while(!q.isEmpty()) {
-			int currentX = q.poll();
-			int currentY = q.poll();
-			
-			if (currentX == endX && currentY == endY) {
-				break;
-			}
-			
-			for (int i = 0; i < 8; i++) {
-				int nextX = currentX + xOffsets[i];
-				int nextY = currentY + yOffsets[i];
-				
-				if (nextX > -1 && nextX < width && nextY > -1 && nextY < width) {
-					if (map[nextX][nextY] != 2) {
-						if (!visited[nextX][nextY]) {
-							visited[nextX][nextY] = true;
-							
-							q.add(nextX);
-							q.add(nextY);
-																					
-							parent[nextX][nextY][0] = currentX;
-							parent[nextX][nextY][1] = currentY;
-						}
-					}
-				}
-			}
-		}
-		
-		int currentX = endX;
-		int currentY = endY;
-		
-		do {
-			MapLocation currentLocation = new MapLocation(currentX, currentY);
-			
-			int parentX = parent[currentX][currentY][0];
-			int parentY = parent[currentX][currentY][1];
-			MapLocation parentLocation = new MapLocation(parentX, parentY);
-			
-			path[parentX][parentY] = parentLocation.directionTo(currentLocation).ordinal();
-			
-			currentX = parentX;
-			currentY = parentY;
-		} while (currentX != startX && currentY != startY);
+
+		if (!isHQPathFound()) {
+			findHQPathTo(rc.senseEnemyHQLocation());
+			printDirectionalField();
+			setHQPathFound();
+		} 
 	}
 	
-	public static void bfs(MapLocation location) throws GameActionException {
-		int width = rc.getMapWidth();
-		int height = rc.getMapHeight();
-		
-		int startX = location.x;
-		int startY = location.y;
-		
-		int xOffsets[] = {-1, 0, 1, 0, -1, 1, 1, -1};
-		int yOffsets[] = {0, -1, 0, 1, 1, -1, 1, -1};
-		
-		LinkedList<Integer> q = new LinkedList<Integer>();
+	public static void moveRandomly() throws GameActionException {
+		Direction movingDirection = directions[(int)(Math.random() * 8)];
+		if (rc.isActive()) {
+			if (rc.canMove(movingDirection)) {
+				rc.move(movingDirection);
+			}
+		}
+	}
+	
+	public static void moveWhereHQWants() throws GameActionException {
+		Direction movingDirection = directions[rc.readBroadcast(locToInt(rc.getLocation()))];
+		if (rc.isActive()) {
+			if (rc.canMove(movingDirection)) {
+				rc.move(movingDirection);
+			} else {
+				//moveRandomly();
+			}
+		}
+	}
+	
+	public static void spawn() throws GameActionException {
+		if (rc.isActive() && rc.canMove(Direction.NORTH)){
+			rc.spawn(Direction.NORTH);
+		}
+	}
+
+	public static void createMap() throws GameActionException {
+		map = new int[width][height];
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				map[x][y] = rc.senseTerrainTile(new MapLocation(x, y)).ordinal();
+			}
+		}
+	}
+
+	public static void setHQPathNotFound() throws GameActionException {
+		rc.broadcast(10000, -1);
+	}
+
+	public static void setHQPathFound() throws GameActionException {
+		rc.broadcast(10000, 1);
+	}
+
+	public static boolean isHQPathFound() throws GameActionException {
+		return rc.readBroadcast(10000) == 1;
+	} 
+
+	public static int locToInt(MapLocation location) throws GameActionException {
+		return location.x * 100 + location.y;
+	}
+
+	public static MapLocation intToLoc(int location) throws GameActionException {
+		return new MapLocation(location / 100, location % 100);
+	}
+
+	public static void findHQPathTo(MapLocation destination) throws GameActionException {
+		int startX = destination.x;
+		int startY = destination.y;
+
+		int xOffsets[] = {-1, -1, -1,  0,  1, 1, 1, 0};
+		int yOffsets[] = { 1,  0, -1, -1, -1, 0, 1, 1};
+
 		boolean visited[][] = new boolean[width][height];
-		
-		for (int i = 0; i < visited.length; i++) {
-			for (int j = 0; j < visited[0].length; j++) {
-				visited[i][j] = false;
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				visited[x][y] = false;
 			}
 		}
 		
+		LinkedList<Integer> q = new LinkedList<Integer>();
+
 		q.add(startX);
 		q.add(startY);
 		q.add(0);
-		visited[startX][startY] = true;
-		
+
 		while (!q.isEmpty()) {
 			int currentX = q.poll();
 			int currentY = q.poll();
-			int time = q.poll();
+			int timeLeft = q.poll();
 			
 			MapLocation currentLocation = new MapLocation(currentX, currentY);
-			
-			if (time == 0) {
+
+			if (timeLeft == 0) {
 				for (int i = 0; i < 8; i++) {
 					int nextX = currentX + xOffsets[i];
 					int nextY = currentY + yOffsets[i];
-					
-					if (nextX > -1 && nextX < width && nextY > -1 && nextY < height) {
-						if (map[nextX][nextY] != 2) {
-							if (!visited[nextX][nextY]) {
-								visited[nextX][nextY] = true;
-								
-								q.add(nextX);
-								q.add(nextY);
-								
-								if (map[nextX][nextY] == 1) {
-									q.add(0);
-								} else {
-									q.add(1);
-								}
-															
-								MapLocation nextLocation = new MapLocation(nextX, nextY);
-															
-								rc.broadcast(locToInt(nextLocation), nextLocation.directionTo(currentLocation).ordinal());
-							}
+
+					if (nextX > -1 && nextY > -1 && nextX < width && nextY < height) {
+						if (!visited[nextX][nextY] && map[nextX][nextY] != 2) {
+							q.add(nextX);
+							q.add(nextY);
+							q.add((map[nextX][nextY] + 1) % 2);
+							
+							visited[nextX][nextY] = true;
+							
+							MapLocation nextLocation = new MapLocation(nextX, nextY);
+							
+							rc.broadcast(locToInt(nextLocation), nextLocation.directionTo(currentLocation).ordinal());
 						}
 					}
 				}
 			} else {
 				q.add(currentX);
 				q.add(currentY);
-				q.add(time - 1);
+				q.add(timeLeft - 1);
 			}
-		}		
+		}
 	}
-
+	
+	public static void printDirectionalField() throws GameActionException {
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				System.out.print(directionToChar(rc.readBroadcast(locToInt(new MapLocation(x,y)))));
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void printMap() throws GameActionException {
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				System.out.print(map[x][y]);
+			}
+			System.out.println();
+		}
+	}
+	
+	public static char directionToChar(int dir) {
+		
+		switch (dir) {
+			case 0:
+				return '>';
+			case 1:
+				return '?';
+			case 2: 
+				return '^';
+			case 3:
+				return '/';
+			case 4:
+				return '\\';
+			case 5:
+				return '?';
+			case 6:
+				return 'v';
+			case 7:
+				return '\\';
+			case 8:
+				return '/';
+			case 9:
+				return '<';
+		}
+		
+		return '?';
+		
+	}
 }
